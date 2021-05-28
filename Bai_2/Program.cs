79f3014ed71b201653ea2cd3e5c61db2055fd88a
realbaotrung <realbaotrung@gmail.com>
2021-05-28 18:27:47 +0700

    Update Bai_2/Program.cs: Print out result to the Console

--- Bai_2/Program.cs
+++ Bai_2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bai_1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,21 @@ namespace Bai_2
 {
     class Program
     {
-        static void Main(string[] args)
+        public static List<Person> GetPersonList() => TaxData.PersonList;
+
+        static void Main()
         {
+            List<Person> people = GetPersonList();
+
+            foreach (var person in people)
+            {
+                person.TaxCoe = TaxData.GetTaxCoe(person);
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"The Id: {person.PersonId} \t|\t Full Name: {person.FullName} \t|\t TaxCoe: {person.TaxCoe} \t|\t Tax: {person.GetTax():C0}");
+            }
         }
     }
 }
