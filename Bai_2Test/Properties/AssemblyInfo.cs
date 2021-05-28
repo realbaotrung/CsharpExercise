870e2c9752d73be4a1f319d40368bff4538575e6
realbaotrung <realbaotrung@gmail.com>
2021-05-28 16:45:03 +0700

    Create folder Bai_2Test, Create and Update TaxDataTest.cs: Add test method GetTaxCoe()

---
+++ Bai_2Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bai_2Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bai_2Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("588814d3-49db-4aec-8037-5ad93c19b145")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bai_2Test/TaxDataTest.cs
@@ -0,0 +1,71 @@
+﻿using Bai_1;
+using Bai_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bai_2Test
+{
+    [TestClass]
+    public class TaxDataTest
+    {
+        [TestMethod]
+        public void GetTaxCoeValid()
+        {
+            //-- Arrange
+            var person1 = new Person()
+            {
+                PersonAge = 16,
+                PersonIncome = 2_600_000M
+
+            };
+
+            var person2 = new Person()
+            {
+                PersonAge = 22,
+                PersonIncome = 7_600_000M
+
+            };
+
+            var person3 = new Person()
+            {
+                PersonAge = 28,
+                PersonIncome = 12_350_000M
+
+            };
+
+            var person4 = new Person()
+            {
+                PersonAge = 31,
+                PersonIncome = 17_500_000M
+
+            };
+
+            var person5 = new Person()
+            {
+                PersonAge = 27,
+                PersonIncome = 22_760_000M
+
+            };
+
+            var expectedPerson1 = 0;
+            var expectedPerson2 = 0.05;
+            var expectedPerson3 = 0.1;
+            var expectedPerson4 = 0.15;
+            var expectedPerson5 = 0.2;
+
+            //-- Act
+            var actualPerson1 = TaxData.GetTaxCoe(person1);
+            var actualPerson2 = TaxData.GetTaxCoe(person2);
+            var actualPerson3 = TaxData.GetTaxCoe(person3);
+            var actualPerson4 = TaxData.GetTaxCoe(person4);
+            var actualPerson5 = TaxData.GetTaxCoe(person5);
+
+            //-- Assert
+            Assert.AreEqual(expectedPerson1, actualPerson1);
+            Assert.AreEqual(expectedPerson2, actualPerson2);
+            Assert.AreEqual(expectedPerson3, actualPerson3);
+            Assert.AreEqual(expectedPerson4, actualPerson4);
+            Assert.AreEqual(expectedPerson5, actualPerson5);
+        }
+    }
+}
