82c508ca87a21845331f19ec5b91c78a891cf0e8
realbaotrung <realbaotrung@gmail.com>
2021-05-28 13:42:08 +0700

    Update Person.cs: create constructors and properties

--- Bai_1/Person.cs
+++ Bai_1/Person.cs
@@ -8,5 +8,43 @@ namespace Bai_1
 {
     public class Person
     {
+        public Person() { }
+
+        public Person(int personId)
+        {
+            PersonId = personId;
+        }
+
+        public int PersonId { get; private set; }
+
+        public string FirstName { get; set; }
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastName))
+                    {
+                        fullName += " ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        public int PersonAge { get; set; }
+
+        public decimal? PersonIncome { get; set; }
+
     }
 }
