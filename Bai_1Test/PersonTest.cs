43cd8acb4772ace59b89eba802dd1cbcccbf0873
realbaotrung <realbaotrung@gmail.com>
2021-05-28 14:55:11 +0700

    Initial PersonTest.cs: Add test property FullName

--- Bai_1Test/PersonTest.cs
+++ Bai_1Test/PersonTest.cs
@@ -7,5 +7,62 @@ namespace Bai_1Test
     [TestClass]
     public class PersonTest
     {
+        [TestMethod]
+        public void FullNameValid()
+        {
+            //-- Arrange
+            var person = new Person()
+            {
+                FirstName = "Trung",
+                LastName = "Nguyen Bao"
+            };
+
+            var expected = "Nguyen Bao Trung";
+
+            //-- Act
+            var actual = person.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            var person = new Person()
+            {
+                LastName = "Nguyen Bao"
+            };
+
+            var expected = "Nguyen Bao";
+
+            //-- Act
+            var actual = person.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            var person = new Person()
+            {
+                FirstName = "Trung"
+            };
+
+            var expected = "Trung";
+
+            //-- Act
+            var actual = person.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
