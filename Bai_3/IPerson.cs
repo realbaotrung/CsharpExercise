fd863169e4247cbc76e879934556aca236904eae
realbaotrung <realbaotrung@gmail.com>
2021-05-31 15:12:26 +0700

    Create project: Bai_3, Add and Edit IPerson.cs

---
+++ Bai_3/IPerson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3
+{
+    public interface IPerson
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+
+        int Age { get; set; }
+
+        decimal? Income { get; set; }
+
+        double TaxCoe { get; set; }
+
+        decimal? GetTax();
+
+        decimal? GetTax(decimal? income, double taxCoe);
+
+        string GetInfo();
+    }
+}
