ffd4812dff82edd1f566bffb3de0be0401698644
realbaotrung <realbaotrung@gmail.com>
2021-05-31 16:28:21 +0700

    Create NameBase.cs for reusable all name properties of related classes

---
+++ Bai_3/NameBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3
+{
+    public class NameBase
+    {
+        public NameBase()
+        {
+
+        }
+        public string LastNameBase { get; set; }
+        public string FirstNameBase { get; set; }
+        public string FullNameBase
+        {
+            get
+            {
+                string fullName = LastNameBase;
+                if (!string.IsNullOrWhiteSpace(FirstNameBase))
+                {
+                    if (!string.IsNullOrWhiteSpace(LastNameBase))
+                    {
+                        fullName += " ";
+                    }
+                    fullName += FirstNameBase;
+                }
+                return fullName;
+            }
+        }
+
+    }
+}
