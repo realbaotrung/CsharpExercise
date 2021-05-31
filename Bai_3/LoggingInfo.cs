9c7d4a5d4020894c9a8969d37998c765aa2abfbe
realbaotrung <realbaotrung@gmail.com>
2021-05-31 21:03:41 +0700

    Create Bai_3/LoggingInfo.cs: the purpose to log the data from List<IPerson> to the console

---
+++ Bai_3/LoggingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3
+{
+    public static class LoggingInfo
+    {
+        public static void WriteToConsole(List<IPerson> itemsToLog)
+        {
+            foreach (var item in itemsToLog)
+            {
+                Console.WriteLine(item.GetInfo());
+            }
+        }
+    }
+}
