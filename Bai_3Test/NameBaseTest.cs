baa730a566f7b434f0397178986e643cc05751b1
realbaotrung <realbaotrung@gmail.com>
2021-05-31 16:29:18 +0700

    Create Bai_3Test project, Add NameBaseTest.cs

---
+++ Bai_3Test/NameBaseTest.cs
@@ -0,0 +1,65 @@
+﻿using Bai_3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Bai_3Test
+{
+    [TestClass]
+    public class NameBaseTest
+    {
+        [TestMethod]
+        public void FullNameValid()
+        {
+            //-- Arrange
+            var personName = new NameBase()
+            {
+                LastNameBase = "Nguyen Bao",
+                FirstNameBase = "Trung",
+            };
+            var expected = "Nguyen Bao Trung";
+
+            //-- Act
+            var actual = personName.FullNameBase;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            var personName = new NameBase()
+            {
+                LastNameBase = "Nguyen Bao",
+            };
+
+            var expected = "Nguyen Bao";
+
+            //-- Act
+            var actual = personName.FullNameBase;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            var personName = new NameBase()
+            {
+                FirstNameBase = "Trung",
+            };
+
+            var expected = "Trung";
+
+            //-- Act
+            var actual = personName.FullNameBase;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Bai_3Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bai_3Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bai_3Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d87cd8d1-4b5a-406e-b01b-467f4d27c02c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
